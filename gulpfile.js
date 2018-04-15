5704607a306a55257be29631b69f975d19c5b5ae
Menasheh Peromsik <menashehyperomsik@gmail.com>
2018-04-15 13:34:03 -0400

    add minified version to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
-let gulp = require('gulp');
 let concat = require('gulp-concat');
-let deporder = require('gulp-deporder');
-let rename = require('gulp-rename');
+let gulp = require('gulp');
 let uglify = require('gulp-uglify');
 
-let jsFiles = ['src/*.js', 'src/morphs/*.js'],
+let jsFiles = [
+        'src/*.js',
+        'src/morphs/*.js'
+    ],
     jsDest = 'dist/';
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
     return gulp.src(jsFiles)
-        .pipe(deporder()) //ordering didn't work without number naming...
         .pipe(concat('morphic.js'))
+        .pipe(gulp.dest(jsDest))
+        .pipe(concat('morphic.min.js'))
+        .pipe(uglify())
         .pipe(gulp.dest(jsDest));
 });
