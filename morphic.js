42ec51d4272426f2997156910ee5093b6c36d99d
jmoenig <jens@moenig.org>
2019-11-12 16:36:04 +0100

    added translation control for submenus
    thanks, Joan!

--- morphic.js
+++ morphic.js
@@ -1178,7 +1178,7 @@
 
 /*global window, HTMLCanvasElement, FileReader, Audio, FileList, Map*/
 
-var morphicVersion = '2019-November-06';
+var morphicVersion = '2019-November-12';
 var modules = {}; // keep track of additional loaded modules
 var useBlurredShadows = getBlurredShadowSupport(); // check for Chrome-bug
 
@@ -8217,12 +8217,34 @@ MenuMorph.prototype.addItem = function (
         verbatim]);
 };
 
-MenuMorph.prototype.addMenu = function (label, aMenu, indicator) {
-    this.addPair(label, aMenu, isNil(indicator) ? '\u25ba' : indicator);
+MenuMorph.prototype.addMenu = function (label, aMenu, indicator, verbatim) {
+    this.addPair(
+        label,
+        aMenu,
+        isNil(indicator) ? '\u25ba' : indicator,
+        null,
+        verbatim // don't translate
+    );
 };
 
-MenuMorph.prototype.addPair = function (label, action, shortcut, hint) {
-    this.addItem(label, action, hint, null, null, null, null, shortcut);
+MenuMorph.prototype.addPair = function (
+    label,
+    action,
+    shortcut,
+    hint,
+    verbatim // don't translate
+) {
+    this.addItem(
+        label,
+        action,
+        hint,
+        null,
+        null,
+        null,
+        null,
+        shortcut,
+        verbatim
+    );
 };
 
 MenuMorph.prototype.addLine = function (width) {
